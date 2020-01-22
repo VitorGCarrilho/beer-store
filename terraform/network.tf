b984925362e3c1db55698229a95fab85e6d31980
Vitor Gabriel Carrilho <vcarrilho@uolinc.com>
2020-01-22 01:13:14 -0300

    provisionando subnets em tres zonas de disponibilidade diferentes

--- terraform/network.tf
+++ terraform/network.tf
@@ -5,3 +5,15 @@ resource "aws_vpc" "main" {
     Name = "vcarrilho-vpc"
   }
 }
+
+resource "aws_subnet" "private_subnet" {
+  count = 3
+
+  cidr_block = "${cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 10)}"
+  vpc_id = "${aws_vpc.main.id}"
+  availability_zone = "${var.avability_zones[count.index]}"
+
+  tags = {
+    Name = "vcarrilho-private-subnet-${count.index}"
+  }
+}
