8b80b4c851778770f10f04caf1f6cd1bfbcf0e81
Vitor Gabriel Carrilho <vcarrilho@uolinc.com>
2020-01-22 23:18:42 -0300

    Adicionando subnets publicas

--- terraform/network.tf
+++ terraform/network.tf
@@ -17,3 +17,16 @@ resource "aws_subnet" "private_subnet" {
     Name = "vcarrilho-private-subnet-${count.index}"
   }
 }
+
+resource "aws_subnet" "public_subnet" {
+  count = 3
+
+  cidr_block = "${cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 20)}"
+  vpc_id = "${aws_vpc.main.id}"
+  availability_zone = "${var.avability_zones[count.index]}"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "vcarrilho-public-subnet-${count.index}"
+  }
+}
