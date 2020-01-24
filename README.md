bb49cd6230da94e6dd61adb26de72ea89c20d414
Vitor Gabriel Carrilho <vcarrilho@uolinc.com>
2020-01-23 23:43:57 -0300

    Adicionando chave keypair a instancia

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Para container com o bd local execute:
 docker run -p 5432:5432 --name beerdb -e POSTGRES_USER=beerstore -e POSTGRES_PASSWORD=beerstore -e POSTGRES_DB=beerstore -d postgres:10.5-alpine
 
 ```
+
+Para gerar uma chave 
+```
+ssh-keygen -t rsa -b 4096 -o -a 100 -f key/beerstore_key
+```
--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,8 +1,13 @@
+resource "aws_key_pair" "key_pair" {
+  public_key = "${file("key/beerstore_key.pub")}"
+}
+
 resource "aws_instance" "instances" {
   count = 3
 
   ami = "ami-062f7200baf2fa504"
   instance_type = "t2.micro"
+  key_name = "${aws_key_pair.key_pair.key_name}"
 
   subnet_id = "${element(aws_subnet.public_subnet.*.id, count.index)}"
 
